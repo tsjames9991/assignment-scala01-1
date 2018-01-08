2f1fbdd7132f9f1d117f6b856c326a2a71fa70bc
Sudeep James Tirkey <sudeep.james@knoldus.in>
2018-01-08 22:59:59 +0530

    Removed All Errors and All Test Cases Passed

--- src/main/scala/edu/knoldus/Searching.scala
+++ src/main/scala/edu/knoldus/Searching.scala
@@ -1,16 +1,44 @@
 package edu.knoldus
 
 class Searching {
+  def bSearch(elements: Array[Int], left: Int, right: Int, key: Int): Int = {
+    if (left > right){
+      return -1
+    }
+    val mid : Int = left + (right - left) / 2
+    if (elements(mid) == key){
+        return mid
+      }
+
+    else if (elements(mid) > key){
+      return bSearch(elements, left, mid - 1, key)
+    }
+    else{
+      return bSearch(elements, mid + 1, right, key)
+    }
+  }
 
   def binarySearch(array: Array[Int], elem: Int): Boolean = {
-    //todo: Add Logic
-    true
+    val Left = 0
+    val Right = array.length - 1
+    val temp = bSearch(array, Left, Right, elem)
+    if (temp != -1)
+      {
+        return true
+      }
+    else
+    {
+      return false
+    }
   }
 
-  def linearSearch(array: Array[Int], elem: Int): Boolean = {
-    //todo: Add Logic
-    true
+  def linearSearch(Elements: Array[Int], elem: Int): Boolean = {
+    for (i <- 0 to Elements.length - 1)
+      {
+        if (Elements(i) == elem)
+          return true
+      }
+    return false
   }
-
 
 }
--- src/main/scala/edu/knoldus/Sorting.scala
+++ src/main/scala/edu/knoldus/Sorting.scala
@@ -2,19 +2,47 @@ package edu.knoldus
 
 class Sorting {
 
-  def insertionSort(array: Array[Int]): Array[Int] = {
-    //todo: Add Logic
-    array
+  def insertionSort(Elements: Array[Int]): Array[Int] = {
+    for( i <- 0 to Elements.length-1)
+    {
+      for(j <- i + 1 to Elements.length - 1)
+        if(Elements(j) < Elements(i))
+        {
+          val temp = Elements(i)
+          Elements(i) = Elements(j)
+          Elements(j) = temp
+        }
+    }
+    Elements
   }
 
-  def selectionSort(array: Array[Int]): Array[Int] = {
-    //todo: Add Logic
-    array
+  def selectionSort(Elements: Array[Int]): Array[Int] = {
+    for( i <- 0 until (Elements.length - 1))
+    {
+      for(j <- 0 until (Elements.length - i - 1 ))
+        if(Elements(j) > Elements(j + 1))
+        {
+          val temp = Elements(j)
+          Elements(j) = Elements(j + 1)
+          Elements(j + 1) = temp
+        }
+    }
+    Elements
   }
 
-  def bubbleSort(array: Array[Int]): Array[Int] = {
-    //todo: Add Logic
-    array
+  def bubbleSort(Elements: Array[Int]): Array[Int] = {
+    for( i <- 0 until Elements.length)
+    {
+      val m = Elements(i)
+      var j = i
+      while (j > 0 && m < Elements(j - 1))
+      {
+        Elements(j) = Elements(j - 1)
+        j -= 1
+      }
+      Elements(j) = m
+    }
+    Elements
   }
 
 }
